44bce2e2495545d6f0b1d06e41cb15214de4662d
Fabrizio V. Guzmán <fabriziovguzman@gmail.com>
2023-11-27 17:16:38 -0300

    Addes final files

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
+function redirectToURL() {
+    window.open("https://github.com/Fabri-GZ", "_blank");
+   }
+
 const links = document.querySelectorAll('a.hvr-underline-from-center');
 
     links.forEach(link => {
         link.addEventListener('click', function(event) {
+            const targetId = link.getAttribute('href');
+            const targetElement = document.querySelector(targetId);
             if (targetElement) {
 
                 window.scrollTo({
